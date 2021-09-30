9d56b50cec7a6b5f1ca6af8d681e519d930fc45c
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-09-29 18:41:18 -0700

    Create button and its behaviour-styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,23 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <button>{props.children}</button>;
+  let buttonClass = 'button';
+
+  if (props.confirm) {
+    buttonClass += ' button--confirm';
+  }
+  
+  if (props.danger) {
+    buttonClass += ' button--danger';
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      disabled={props.disabled}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
